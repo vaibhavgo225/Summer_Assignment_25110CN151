0f01c7f190cf0203ea1d96a1b2b516567c09045f
Vaibhav Goswami <vaibhavana225@gmail.com>
2026-07-04 16:16:27 +0530

    Day-30 Uploaded

--- DAY-30/Ques-120.cpp
+++ DAY-30/Ques-120.cpp
@@ -1,52 +1,157 @@
-#include <iostream>
-#include <string>
+//EMI calculator and loan management system
+
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-int main() {
-    string options[6] = {"Music", "Gaming", "Sports", "Reading", "Travel", "Movies"};
-    bool person1[6] = {false};
-    bool person2[6] = {false};
-    string name1, name2;
+int main(){
+
+    string name[50];
+    int loanID[50];
+    float loanAmount[50],interestRate[50],years[50],emi[50];
+
+    int loans=0;
     int choice;
 
-    cout << "Interest options:\n";
-    for (int i = 0; i < 6; i++) {
-        cout << i + 1 << ". " << options[i] << "\n";
-    }
+    do{
 
-    cout << "\nEnter first person's name: ";
-    cin >> name1;
-    cout << "Enter interest numbers for " << name1 << " (0 to stop): ";
-    while (cin >> choice && choice != 0) {
-        if (choice >= 1 && choice <= 6) {
-            person1[choice - 1] = true;
+        cout<<"\n====== EMI CALCULATOR SYSTEM ======\n";
+        cout<<"1. Add Loan\n";
+        cout<<"2. View Loans\n";
+        cout<<"3. Search Loan\n";
+        cout<<"4. Delete Loan\n";
+        cout<<"5. Exit\n";
+        cout<<"Enter your choice : ";
+        cin>>choice;
+        cin.ignore();
+
+        if(choice==1){
+
+            cout<<"\nEnter Customer Name : ";
+            getline(cin,name[loans]);
+
+            cout<<"Enter Loan ID : ";
+            cin>>loanID[loans];
+
+            cout<<"Enter Loan Amount : ";
+            cin>>loanAmount[loans];
+
+            cout<<"Enter Annual Interest Rate (%) : ";
+            cin>>interestRate[loans];
+
+            cout<<"Enter Loan Duration (Years) : ";
+            cin>>years[loans];
+
+            float r=interestRate[loans]/12/100;
+            int n=years[loans]*12;
+
+            emi[loans]=(loanAmount[loans]*r*pow(1+r,n))/(pow(1+r,n)-1);
+
+            loans++;
+
+            cout<<"\nLoan Added Successfully.\n";
         }
-    }
 
-    cout << "\nEnter second person's name: ";
-    cin >> name2;
-    cout << "Enter interest numbers for " << name2 << " (0 to stop): ";
-    while (cin >> choice && choice != 0) {
-        if (choice >= 1 && choice <= 6) {
-            person2[choice - 1] = true;
+        else if(choice==2){
+
+            if(loans==0){
+
+                cout<<"\nNo Records Found.\n";
+            }
+
+            else{
+
+                cout<<"\n------ LOAN RECORDS ------\n";
+
+                for(int i=0;i<loans;i++){
+
+                    cout<<"\nCustomer "<<i+1<<endl;
+                    cout<<"Name : "<<name[i]<<endl;
+                    cout<<"Loan ID : "<<loanID[i]<<endl;
+                    cout<<"Loan Amount : "<<loanAmount[i]<<endl;
+                    cout<<"Interest Rate : "<<interestRate[i]<<"%"<<endl;
+                    cout<<"Duration : "<<years[i]<<" Years"<<endl;
+                    cout<<"Monthly EMI : "<<emi[i]<<endl;
+                }
+            }
         }
-    }
 
-    int matchCount = 0, totalCount = 0;
-    cout << "\nCommon interests:\n";
-    for (int i = 0; i < 6; i++) {
-        if (person1[i] && person2[i]) {
-            matchCount++;
-            cout << "- " << options[i] << "\n";
+        else if(choice==3){
+
+            int searchID;
+            bool found=false;
+
+            cout<<"\nEnter Loan ID : ";
+            cin>>searchID;
+
+            for(int i=0;i<loans;i++){
+
+                if(loanID[i]==searchID){
+
+                    cout<<"\nLoan Found\n";
+                    cout<<"Customer : "<<name[i]<<endl;
+                    cout<<"Loan Amount : "<<loanAmount[i]<<endl;
+                    cout<<"Monthly EMI : "<<emi[i]<<endl;
+
+                    found=true;
+                    break;
+                }
+            }
+
+            if(found==false){
+
+                cout<<"\nLoan Not Found.\n";
+            }
         }
-        if (person1[i] || person2[i]) {
-            totalCount++;
+
+        else if(choice==4){
+
+            int deleteID;
+            bool found=false;
+
+            cout<<"\nEnter Loan ID : ";
+            cin>>deleteID;
+
+            for(int i=0;i<loans;i++){
+
+                if(loanID[i]==deleteID){
+
+                    for(int j=i;j<loans-1;j++){
+
+                        name[j]=name[j+1];
+                        loanID[j]=loanID[j+1];
+                        loanAmount[j]=loanAmount[j+1];
+                        interestRate[j]=interestRate[j+1];
+                        years[j]=years[j+1];
+                        emi[j]=emi[j+1];
+                    }
+
+                    loans--;
+
+                    found=true;
+
+                    cout<<"\nLoan Record Deleted Successfully.\n";
+                    break;
+                }
+            }
+
+            if(found==false){
+
+                cout<<"\nLoan Not Found.\n";
+            }
         }
-    }
 
-    float percentage = totalCount == 0 ? 0 : ((float)matchCount / totalCount) * 100;
+        else if(choice==5){
 
-    cout << "\nFriendship Match: " << percentage << "%\n";
+            cout<<"\nThank You!\n";
+        }
+
+        else{
+
+            cout<<"\nInvalid Choice.\n";
+        }
+
+    }while(choice!=5);
 
     return 0;
 }
